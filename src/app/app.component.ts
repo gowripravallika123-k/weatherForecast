b1d8945792cf26635181e1e376cca23654e4e2f8
gowripravallika <gowripravallika123@gmail.com>
2022-12-30 21:26:09 +0530

    Initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather';
+  city:any;
+  weatherData:any=[];
+  loading:boolean=false
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit(){
+  }
+  getForecast(){
+    this.loading=true;
+    this.http.get(`https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=5613f3680a502bff3fe9021e145908cb`).subscribe(
+      (res) => {
+        this.loading=false;
+       let temp:any=res;
+      this.weatherData=temp?.list.slice(0,5);
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {InputTextModule} from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ButtonModule,
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
